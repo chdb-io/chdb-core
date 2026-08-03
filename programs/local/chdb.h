0b647afc781203966136d89e8d7bb02290411eda
wudidapaopao <xiaozhe.yu@clickhouse.com>
2026-08-03 09:09:40 +0000

    Add a concise doc comment for chdb_version()

--- programs/local/chdb.h
+++ programs/local/chdb.h
@@ -14,6 +14,10 @@ extern "C" {
 
 #define CHDB_VERSION "26.5.1-rc.3"
 
+/**
+ * Returns the version of the linked chDB library (no connection required).
+ * @return Null-terminated version string, e.g. "26.5.1-rc.3"
+ */
 CHDB_EXPORT const char * chdb_version(void);
 
 #ifndef CHDB_NO_DEPRECATED
